4cfefddfcc5f85086e0b70de023d3ab665a3091b
HootOwl17 <davidscout2010@hotmail.com>
2022-06-11 19:38:14 -0600

    Agregando más páginas
    Se agregaron otra paginas que también se utilizarán.
    De paso cambiando *.html a *.aspx

---
+++ forgot-password.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class forgot_password
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+End Class
