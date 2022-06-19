68bb586bddb22c0825fce74d6a4463af9d88c107
HootOwl17 <davidscout2010@hotmail.com>
2022-06-18 18:30:57 -0600

    Intentando hacer login

---
+++ Conectar.vb
@@ -0,0 +1,16 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Module Conectar
+    Public Conexion As New MySqlConnection
+
+    Public Sub ConectarMySql()
+        Try
+            With Conexion
+                .ConnectionString = "server=localhost;User Id=root;database=sistemagestion;Password= ;SslMode=none"
+                .Open()
+            End With
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Module
