2cf8cff17965e1f6c68c6bb80d8aa13aef5cc5b0
HootOwl17 <davidscout2010@hotmail.com>
2022-07-24 10:37:13 -0600

    Update Conectar.vb
    Comentar modulo no utilizado

--- Conectar.vb
+++ Conectar.vb
@@ -1,24 +1,6 @@
 ﻿Imports System.Web
 Imports MySql.Data.MySqlClient
 
-Public Module Conectar
-    Public Conexion As New MySqlConnection
-    Dim comando As MySqlCommand = New MySqlCommand
-    'Public Shared Cnn As SqlClient.SqlConnection
-    'Public Shared Validar As String = "0"
-
-    Public Sub ConectarMySql()
-        Try
-            With Conexion
-                .ConnectionString = "server=localhost;User Id=root;database=sistemagestion;Password= ;SslMode=none"
-                .Open()
-            End With
-        Catch ex As Exception
-            MsgBox(ex.Message)
-        End Try
-    End Sub
-End Module
-
 Public Class Conexiones
     'Public 
     Public Shared Cnn As MySqlConnection
@@ -30,3 +12,21 @@ Public Class Conexiones
         'Cnn.ConnectionString = "server=localhost:3306;User Id=root;database=sistemagestion;Password= ;" 'SslMode=none
     End Sub
 End Class
+
+'Public Module Conectar
+'    Public Conexion As New MySqlConnection
+'    Dim comando As MySqlCommand = New MySqlCommand
+'    'Public Shared Cnn As SqlClient.SqlConnection
+'    'Public Shared Validar As String = "0"
+
+'    Public Sub ConectarMySql()
+'        Try
+'            With Conexion
+'                .ConnectionString = "server=localhost;User Id=root;database=sistemagestion;Password= ;SslMode=none"
+'                .Open()
+'            End With
+'        Catch ex As Exception
+'            MsgBox(ex.Message)
+'        End Try
+'    End Sub
+'End Module
